12014279373641e9e178006a1a90075d549c1b71
Mburu540 <edupablo72@gmail.com>
2025-08-27 07:58:36 +0300

    Updates 2025-08-27:3

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import os
 from contextlib import asynccontextmanager
 
+import uvicorn
 from fastapi import FastAPI
-from starlette.middleware.cors import CORSMiddleware
 from api import chat
 from core.database import init_db
 from docops.docops import load_and_index
@@ -15,12 +16,13 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 app.include_router(chat.router)
+
+@app.get('/health')
+def health():
+    return "OK", 200
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
+
